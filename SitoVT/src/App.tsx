9b5f9d593933c057069dba645767239d6ccbf3ab
mirko <mirkocerasi@hotmail.com>
2023-07-15 18:04:37 +0200

    update

--- SitoVT/src/App.tsx
+++ SitoVT/src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import Navbar from './Navbar';
-import Sidebar from './Sidebar';
+import Sidebar, {menuItems, MenuItem} from './Sidebar';
 import './style.scss';
 import { Layout } from 'antd';
+import { RouteProps } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+const pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
+
+const routes = [{path: '404.tsx', component: 'error'}];
+for (const path of Object.keys(pages)) {
+  const fileName = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1];
+  if (!fileName) {
+    continue;
+  }
+
+    routes.push({
+    path: path,
+    component: fileName    
+  });
+  console.log(fileName);
+
+}
+
 const App: React.FC = () => {
 
   return (
@@ -11,6 +32,17 @@ const App: React.FC = () => {
       <Sidebar />
       <Layout className="sitelayout">
         <h1>ciao, lo sai che ho creato un sito appositamente per te!</h1>
+        <Router>
+          <Routes>
+            {routes.map((router, index) => (
+          <Route
+            key={router.path}
+            path={router.path}
+            element={<router.component />}
+          />
+          ))}
+          </Routes>
+        </Router>
       </Layout>
     </div>
   );
---
+++ SitoVT/src/pages/home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function home() {
+    return <h1>Home Page</h1>;
+}
+
+export default home;
